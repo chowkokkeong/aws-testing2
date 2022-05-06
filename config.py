5eadbfd292a30bf02456fc6ab776a420b854ff26
chowkokkeong <50937950+chowkokkeong@users.noreply.github.com>
2022-05-06 14:44:18 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.clifhgpky1m3.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "cpac3009"
-customdb = "employee"
-custombucket = "chowkokkeong-employee"
-customregion = "us-east-1"
